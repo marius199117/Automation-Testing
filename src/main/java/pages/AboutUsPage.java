6beb6468ffad7520b70946df3359dcd42084d0bd
marius199117 <marius199117@gmail.com>
2018-12-24 16:16:21 +0200

    d

--- src/main/java/pages/AboutUsPage.java
+++ src/main/java/pages/AboutUsPage.java
@@ -47,9 +47,9 @@ public class AboutUsPage extends Helper {
         Actions act = new Actions(this.getDriver());
         SoftAssertions softAssertion = new SoftAssertions();
         act.moveToElement(this.getDriver().findElement(aboutUs())).click().build().perform();
-        act.moveToElement(this.getDriver().findElement(afterClick()));
         Assert.assertTrue("Fail",afterClick.isDisplayed());
         act.moveToElement(this.getDriver().findElement(afterClick())).click().build().perform();
+        Assert.assertTrue("No",afterVideoClick.isDisplayed() );
         waitABit(3000);
 
 //        clickAndAssert(aboutUs, afterClick, "About Us button is not functional");
