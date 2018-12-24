b07e38a25cbb1e7bae08f13cc592944f3e65a044
marius199117 <marius199117@gmail.com>
2018-12-24 15:44:11 +0200

    da

--- src/main/java/pages/AboutUsPage.java
+++ src/main/java/pages/AboutUsPage.java
@@ -1,8 +1,11 @@
 package pages;
 
 import common.Helper;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class AboutUsPage extends Helper {
@@ -22,22 +25,61 @@ public class AboutUsPage extends Helper {
     @FindBy(xpath = "/html/body/div[4]/div/div/div[3]/button")
     WebElement selectCloseButton;
 
+    public static By aboutUs() {
+        return By.cssSelector("a.nav-link[data-target*=\"#videoModal\"]");
+    }
+
+    public static By afterClick() {
+        return By.className("vjs-poster");
+    }
+
+    public static By xButton() {
+        return By.xpath("/html/body/div[4]/div/div/div[1]/button");
+    }
+
+    public static By closeButton() {
+        return By.xpath("/html/body/div[4]/div/div/div[3]/button");
+    }
+
     // Test 1 : Select About Us and Select the video from the About Us section
     public void selectAboutUs() {
-        clickAndAssert(aboutUs, afterClick, "About Us button is not functional");
-        clickAndAssert(afterClick, afterVideoClick, "Video is not functional");
+
+        Actions act = new Actions(this.getDriver());
+        SoftAssertions softAssertion = new SoftAssertions();
+        act.moveToElement(this.getDriver().findElement(aboutUs())).click().build().perform();
+        Assert.assertTrue("Fail", afterClick.isDisplayed());
+        act.moveToElement(this.getDriver().findElement(afterClick())).click().build().perform();
+        waitABit(3000);
+
+//        clickAndAssert(aboutUs, afterClick, "About Us button is not functional");
+//        waitABit(2000);
+//        clickAndAssert(afterClick, afterVideoClick, "Video is not functional");
     }
 
-    //    // Test 2 : Select About Us and X button
+    // Test 2 : Select About Us and X button
     public void selectAboutUsXbutton() {
-        clickAndAssert(aboutUs, afterClick, "About Us button is not functional");
-        click(selectXButton);
+
+        Actions act = new Actions(this.getDriver());
+        act.moveToElement(this.getDriver().findElement(aboutUs())).click().build().perform();
+        waitABit(3000);
+        act.moveToElement(this.getDriver().findElement(xButton())).click().build().perform();
+        waitABit(3000);
+//        clickAndAssert(aboutUs, afterClick, "About Us button is not functional");
+//        click(selectXButton);
     }
 
+    //
     // Test 3 : Select About Us and Close button
     public void selectCloseAboutUs() {
-        clickAndAssert(aboutUs, afterClick, "About Us button is not functional");
-        click(selectCloseButton);
+        Actions act = new Actions(this.getDriver());
+        act.moveToElement(this.getDriver().findElement(aboutUs())).click().build().perform();
+        waitABit(3000);
+        act.moveToElement(this.getDriver().findElement(closeButton())).click().build().perform();
+        waitABit(3000);
+//        clickAndAssert(aboutUs, afterClick, "About Us button is not functional");
+//        click(selectCloseButton);
+
+
     }
 }
 
