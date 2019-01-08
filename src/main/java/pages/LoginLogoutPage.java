6d5f718ae44cdd7960b0e61d280c4c8124173bb5
marius199117 <marius199117@gmail.com>
2019-01-08 21:30:16 +0200

    login update

--- src/main/java/pages/LoginLogoutPage.java
+++ src/main/java/pages/LoginLogoutPage.java
@@ -34,6 +34,9 @@ public class LoginLogoutPage extends Helper2 {
     @FindBy(id = "logout2")
     WebElement logout;
 
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]")
+    WebElement afterLoginElements;
+
     public static By logIn() {
         return By.cssSelector("a.nav-link[data-target*=\"#logInModal\"]");
     }
@@ -53,69 +56,83 @@ public class LoginLogoutPage extends Helper2 {
     public static By logout() {
         return By.id("logout2");
     }
+        public static By afterLoginElements () {
+            return By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]");
+        }
 
-    // Test 1: Login button functionality and elements are present
-    public void loginElements() {
-        clickElement(this.getDriver(), logIn(), 3000);
-        Assert.assertTrue("Login Button is not functional", logIn.isDisplayed());
-        Assert.assertTrue("Username field is not displayed", username.isDisplayed());
-        Assert.assertTrue("Password field is not displayed", password.isDisplayed());
-        Assert.assertTrue("Close Button is not displayed", closeButton.isDisplayed());
-        Assert.assertTrue("Login Button is not displayed", loginButton.isDisplayed());
+        // Test 1: Login button functionality and elements are present
+        public void loginElements () {
+            clickElement(this.getDriver(), logIn(), 3000);
+            Assert.assertTrue("Login Button is not functional", logIn.isDisplayed());
+            Assert.assertTrue("Username field is not displayed", username.isDisplayed());
+            Assert.assertTrue("Password field is not displayed", password.isDisplayed());
+            Assert.assertTrue("Close Button is not displayed", closeButton.isDisplayed());
+            Assert.assertTrue("Login Button is not displayed", loginButton.isDisplayed());
+        }
+
+        // Test 2: Successful login and logout
+        public void successfulLogin (String user, String pass){
+            clickElement(this.getDriver(), logIn(), 3000);
+            element(afterLoginElements()).withTimeoutOf(15, TimeUnit.SECONDS).waitUntilVisible();
+            Assert.assertTrue("Sign Up is not functional", afterLoginElements.isDisplayed());
+            clickAndSendKeys(this.getDriver(), username(), user, 3000);
+            clickAndSendKeys(this.getDriver(), password(), pass, 3000);
+            clickElement(this.getDriver(), logInButton(), 3000);
+            Assert.assertTrue("Login Confirmation is not functional", loginConfirmation.isDisplayed());
+            clickElement(this.getDriver(), logout(), 3000);
+            Assert.assertTrue("The user is not logged out", logIn.isDisplayed());
+
+        }
+
+        // Test 3: Empty fields login
+        public void emptyFieldsLogin () {
+            clickElement(this.getDriver(), logIn(), 3000);
+            element(afterLoginElements()).withTimeoutOf(15, TimeUnit.SECONDS).waitUntilVisible();
+            Assert.assertTrue("Sign Up is not functional", afterLoginElements.isDisplayed());
+            clickElement(this.getDriver(), logInButton(), 3000);
+        }
+
+        // Test 4: Empty field for username
+        public void emptyUsernameField (String pass){
+            clickElement(this.getDriver(), logIn(), 3000);
+            element(afterLoginElements()).withTimeoutOf(15, TimeUnit.SECONDS).waitUntilVisible();
+            Assert.assertTrue("Sign Up is not functional", afterLoginElements.isDisplayed());
+            clickAndSendKeys(this.getDriver(), password(), pass, 3000);
+            clickElement(this.getDriver(), logInButton(), 3000);
+        }
+
+        // Test 5: Empty field for password
+        public void emptyPasswordField (String user){
+            clickElement(this.getDriver(), logIn(), 3000);
+            element(afterLoginElements()).withTimeoutOf(15, TimeUnit.SECONDS).waitUntilVisible();
+            Assert.assertTrue("Sign Up is not functional", afterLoginElements.isDisplayed());
+            clickAndSendKeys(this.getDriver(), username(), user, 3000);
+            clickElement(this.getDriver(), logInButton(), 3000);
+        }
+
+        // Test 5: Invalid credentials (invalid username and password credentials)
+        public void invalidCredentials (UserInfo userInfo){
+            clickElement(this.getDriver(), logIn(), 3000);
+            element(afterLoginElements()).withTimeoutOf(15, TimeUnit.SECONDS).waitUntilVisible();
+            Assert.assertTrue("Sign Up is not functional", afterLoginElements.isDisplayed());
+            clickAndSendKeys(this.getDriver(), username(), userInfo.username, 3000);
+            clickAndSendKeys(this.getDriver(), password(), userInfo.password, 3000);
+        }
+
+        // Test 6: Invalid credentials (valid username and password with a space afterwards)
+        public void usernamePasswordSpaceAfterwards (UserInfo userInfo){
+            clickElement(this.getDriver(), logIn(), 3000);
+            element(afterLoginElements()).withTimeoutOf(15, TimeUnit.SECONDS).waitUntilVisible();
+            Assert.assertTrue("Sign Up is not functional", afterLoginElements.isDisplayed());
+            clickAndSendKeys(this.getDriver(), username(), userInfo.validUsernameBlankSpace, 3000);
+            clickAndSendKeys(this.getDriver(), password(), userInfo.validPasswordBlankSpace, 3000);
+            clickElement(this.getDriver(), logInButton(), 3000);
+        }
+
+        // Test 7: Invalid credentials (
+
+
     }
-
-    // Test 2: Successful login and logout
-    public void successfulLogin(String user, String pass) {
-        clickElement(this.getDriver(), logIn(), 3000);
-        clickAndSendKeys(this.getDriver(), username(), user, 3000);
-        clickAndSendKeys(this.getDriver(), password(), pass, 3000);
-        clickElement(this.getDriver(), logInButton(), 3000);
-        Assert.assertTrue("Login Confirmation is not functional", loginConfirmation.isDisplayed());
-        clickElement(this.getDriver(), logout(), 3000);
-        Assert.assertTrue("The user is not logged out", logIn.isDisplayed());
-
-    }
-
-    // Test 3: Empty fields login
-    public void emptyFieldsLogin() {
-        clickElement(this.getDriver(), logIn(), 3000);
-        Assert.assertTrue("Login Button is not functional", logIn.isDisplayed());
-        clickElement(this.getDriver(), logInButton(), 3000);
-    }
-
-    // Test 4: Empty field for username
-    public void emptyUsernameField(String pass) {
-        clickElement(this.getDriver(), logIn(), 3000);
-        clickAndSendKeys(this.getDriver(), password(), pass, 3000);
-        clickElement(this.getDriver(), logInButton(), 3000);
-    }
-
-    // Test 5: Empty field for password
-    public void emptyPasswordField(String user) {
-        clickElement(this.getDriver(), logIn(), 3000);
-        clickAndSendKeys(this.getDriver(), username(), user, 3000);
-        clickElement(this.getDriver(), logInButton(), 3000);
-    }
-
-    // Test 5: Invalid credentials (invalid username and password credentials)
-    public void invalidCredentials(UserInfo userInfo) {
-        clickElement(this.getDriver(), logIn(), 3000);
-        clickAndSendKeys(this.getDriver(), username(), userInfo.username, 3000);
-        clickAndSendKeys(this.getDriver(), password(), userInfo.password, 3000);
-    }
-
-    // Test 6: Invalid credentials (valid username and password with a space afterwards)
-    public void usernamePasswordSpaceAfterwards(UserInfo userInfo) {
-        clickElement(this.getDriver(), logIn(), 3000);
-        clickAndSendKeys(this.getDriver(), username(), userInfo.validUsernameBlankSpace, 3000);
-        clickAndSendKeys(this.getDriver(), password(), userInfo.validPasswordBlankSpace, 3000);
-        clickElement(this.getDriver(), logInButton(), 3000);
-    }
-
-    // Test 7: Invalid credentials (
-
-
-}
 
 
 
